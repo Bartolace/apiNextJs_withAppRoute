227a275a212c22c4b18e0c32f58e1d7480edddc2
Bartolace <gabrielll.freitas@hotmail.com>
2024-04-10 22:50:58 -0400

    Update route.ts

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,3 +1,14 @@
-export async function GET(request: Request) {
+import { NextResponse } from "next/server"
+
+
+export async function GET(request: Request, response: NextResponse) {
   console.log("GET RESQUEST")
+  return NextResponse.json({ sucess: 'foi bobo' }, { status: 200 })
+  
 }
+
+export async function POST(request: Request) {}
+ 
+export async function PUT(request: Request) {}
+ 
+export async function DELETE(request: Request) {}
